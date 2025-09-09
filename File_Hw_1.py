8ffbf2a579cd69576b7264b25cd65c56caab041f
Matthewh1225 <mhuckman97@yahoo.com>
2025-09-09 12:23:23 -0500

    combine Functions

--- File_Hw_1.py
+++ File_Hw_1.py
@@ -1,17 +1,27 @@
 file_0 = "file0.txt" # original names
 file_1 = "file1.txt" # reversed names
-file_2 = "file2.txt" # re-reversed names
+file_2 = "file2.txt" # restored names
 
-#read files 1 char at a time and prints to console
-def read_file_byChar():
-    File_to_find = input("Enter the name of the file to read text from:   ")
-    with open(File_to_find,"r") as file:
-        while True:
-                c = file.read(1)
-                print(c,end="")   
-                if not c:
-                    break     
-    file.close()  
+def read_file_byChar(file_name = None):
+    #reads 1 char at a time and prints to console
+    if file_name is None:
+        File_to_Read = input("Enter the name of the file to read text from:   ")
+        with open(File_to_Read,"r") as file:
+            while True:
+                    c = file.read(1)
+                    print(c,end="")   
+                    if not c:
+                        break    
+        file.close()
+    # returns list of chars from file
+    if file_name is not None:
+        chars = []
+        with open(file_name,"r") as file:
+                for name in file:
+                    for char in name:
+                        chars.append(char)
+        file.close()  
+        return chars   
 
 #gets names from user input,adds name to list & prints each name reversed
 def get_names():
@@ -26,18 +36,9 @@ def get_names():
             names_list.append(name)       
     return names_list
 
-def Collect_Chars(file_name):
-    chars = []
-    with open(file_name,"r") as file:
-            for name in file:
-                for char in name:
-                    chars.append(char)
-    file.close()  
-    return chars
-
 #uses stack/ppop to reverse names
 def reverse_names(file_name):
-    char_list = Collect_Chars(file_name)
+    char_list = read_file_byChar(file_name)
     names = []
     name = ""
     for char in char_list:
@@ -53,15 +54,17 @@ def reverse_names(file_name):
             name += char
     return names
 
-def write_reverse_sort_names():
-    #write names to file0
+def Assignment(): 
+    read_file_byChar()#read file char by char & print to console
+
+    #write normal names to file_0
     Original_names=get_names()
-    with open(file_0,"w") as file_0:
+    with open(file_0,"w") as file0:
         for name in Original_names:
-                file_0.write(name + "\n")
+                file0.write(name + "\n")
     print("\n Names written to", file_0, "\n")  
-    
-    file_0.close()
+    file0.close()
+
     #read from File 0 & write reverse names into file_1
     rev_names = reverse_names(file_0)   
     with open(file_1, "w") as file1:
@@ -70,13 +73,13 @@ def write_reverse_sort_names():
     file1.close()    
     print(" Reversed names written to", file_1,"\n")
 
-     #read from File 1 & write restored names into file_2
+    #read from File 1 & write restored names into file_2
     restore_names = reverse_names(file_1)
     with open(file_2, "w") as file2:
         for name in restore_names:
             file2.write(name + "\n")
     file2.close()      
-    print(" Re-reversed names written to", file_2,"\n")
+    print(" Restored names written to", file_2,"\n")
 
     #read from file_2, reverse, sort & print names
     names = reverse_names(file_2)
@@ -84,13 +87,3 @@ def write_reverse_sort_names():
     print("\n---reversed & Sorted--- ")
     for name in names:
         print("   ",name)
-
-# read_file_byChar()
-# Write_names()
-write_reverse_names()
-write_re_reverse()
-reverse_sort()
-#question 3 = 21??
-# 25000*256 = 6,400,000
-# 512*50*12 = 307,200
-# 6,400,000/307,200=20.8->21
