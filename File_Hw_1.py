8d8c5cdd23998862e62605f7d7bd96285008f6c4
Matthewh1225 <mhuckman97@yahoo.com>
2025-09-09 13:44:06 -0500

    update Main Functions

--- File_Hw_1.py
+++ File_Hw_1.py
@@ -5,14 +5,16 @@ file_2 = "file2.txt" # restored names
 def read_file_byChar(file_name = None):
     #reads 1 char at a time and prints to console
     if file_name is None:
-        File_to_Read = input("Enter the name of the file to read text from:   ")
-        with open(File_to_Read,"r") as file:
+        File_to_Read = input("Enter the name of the file to read text from: ")
+        with open(File_to_Read,"r") as file_R:
             while True:
-                    c = file.read(1)
-                    print(c,end="")   
+                    c = file_R.read(1)
+                    print(c,end="")  
                     if not c:
+                        print("")
                         break    
-        file.close()
+        file_R.close()
+        print("")
     # returns list of chars from file
     if file_name is not None:
         chars = []
@@ -27,8 +29,8 @@ def read_file_byChar(file_name = None):
 def get_names():
     names_list = []
     getting = True
-    while (getting):  
-        name = input("\nType names or click e to exit!").strip()
+    while (getting): 
+        name = input("Enter a name or click (E) to exit!").strip()
         if name.lower() == "e":
             getting = False
         else:
@@ -57,33 +59,32 @@ def reverse_names(file_name):
 def Assignment(): 
     read_file_byChar()#read file char by char & print to console
 
-    #write normal names to file_0
+    #write user typed names to file_0
     Original_names=get_names()
     with open(file_0,"w") as file0:
         for name in Original_names:
                 file0.write(name + "\n")
-    print("\n Names written to", file_0, "\n")  
     file0.close()
 
-    #read from File 0 & write reverse names into file_1
+    #read names from File 0 & write reverse names into file_1
     rev_names = reverse_names(file_0)   
     with open(file_1, "w") as file1:
         for name in rev_names:
             file1.write(name + "\n")
     file1.close()    
-    print(" Reversed names written to", file_1,"\n")
 
-    #read from File 1 & write restored names into file_2
+    #read names from File 1 & write restored names into file_2
     restore_names = reverse_names(file_1)
     with open(file_2, "w") as file2:
         for name in restore_names:
             file2.write(name + "\n")
     file2.close()      
-    print(" Restored names written to", file_2,"\n")
 
-    #read from file_2, reverse, sort & print names
+    #read names from file_2, reverse, sort & print names
     names = reverse_names(file_2)
     names.sort()
-    print("\n---reversed & Sorted--- ")
+    print("")
     for name in names:
         print("   ",name)
+
+Assignment()
